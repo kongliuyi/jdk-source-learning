b8479bd3874dcceb5f870b00449de3f509ce156c
kongliuyi <315294970@qqq.com>
2020-04-20 20:16:24 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Description
+ * @Author: kongLiuYi
+ * @Date: 2020/3/23 0023 20:47
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("编译测试");
+    }
+}
